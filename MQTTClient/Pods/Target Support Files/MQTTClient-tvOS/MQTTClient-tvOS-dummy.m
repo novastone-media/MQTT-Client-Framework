94240e91f092b64159a16a58fb807aebccff4d76
Josip Cavar <josipcavar04@gmail.com>
2018-04-24 18:58:15 +0200

    Remove unnecessary flags for macOS

--- MQTTClient/Pods/Target Support Files/MQTTClient-tvOS/MQTTClient-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MQTTClient_tvOS : NSObject
-@end
-@implementation PodsDummy_MQTTClient_tvOS
-@end
--- MQTTClient/Pods/Target Support Files/Pods-MQTTClienttvOSTests/Pods-MQTTClienttvOSTests-frameworks.sh
+++ MQTTClient/Pods/Target Support Files/Pods-MQTTClienttvOSTests/Pods-MQTTClienttvOSTests-frameworks.sh
@@ -102,12 +102,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/MQTTClient-tvOS/MQTTClient.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SocketRocket-tvOS/SocketRocket.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MQTTClient/MQTTClient.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SocketRocket/SocketRocket.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/MQTTClient-tvOS/MQTTClient.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SocketRocket-tvOS/SocketRocket.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MQTTClient/MQTTClient.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SocketRocket/SocketRocket.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
