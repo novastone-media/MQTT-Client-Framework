1611d4e5cc45d7738ea4e3001b0d6d8827234bd8
Josip Cavar <josipcavar04@gmail.com>
2018-04-25 08:49:13 +0200

    Add fix for crash when decoder deallocated
    This is related to previous crash:
    https://github.com/novastone-media/MQTT-Client-Framework/issues/325

--- MQTTClient/MQTTClient/MQTTDecoder.m
+++ MQTTClient/MQTTClient/MQTTDecoder.m
@@ -93,6 +93,13 @@
 }
 
 - (void)stream:(NSStream *)sender handleEvent:(NSStreamEvent)eventCode {
+    // We contact our delegate, MQTTSession at some point in this method
+    // This call can cause MQTTSession to dealloc and thus, MQTTDecoder to dealloc
+    // So we end up with invalid object in the middle of the method
+    // To prevent this we retain self for duration of this method call
+    MQTTDecoder *strongDecoder = self;
+    (void)strongDecoder;
+    
     NSInputStream *stream = (NSInputStream *)sender;
     
     if (eventCode & NSStreamEventOpenCompleted) {
