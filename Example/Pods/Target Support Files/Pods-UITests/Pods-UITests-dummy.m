0d2803af863e2990733839ebe99a0b5a9495fe70
Josip Cavar <josipcavar04@gmail.com>
2019-10-15 11:41:41 +0200

    Fix pods issues to get uitests target to run

---
+++ Example/Pods/Target Support Files/Pods-UITests/Pods-UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UITests : NSObject
+@end
+@implementation PodsDummy_Pods_UITests
+@end
--- Example/Pods/Target Support Files/Pods-UITests/Pods-UITests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-UITests/Pods-UITests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_MQTTSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MQTTSwiftVersionString[];
+FOUNDATION_EXPORT double Pods_UITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UITestsVersionString[];
 
