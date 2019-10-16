67d59aea708347fb38c3d3992883f93869786247
Josip Cavar <josipcavar04@gmail.com>
2019-10-16 10:17:29 +0200

    Remove unused script

--- run_browserstack.sh
+++
@@ -1,7 +0,0 @@
-xcodebuild -destination generic/platform=iOS -workspace Example/Example.xcworkspace -scheme "Example" -derivedDataPath "build" CODE_SIGNING_ALLOWED=NO
-#mkdir Payload
-#cp -r build/Build/Products/Debug-iphoneos/Example.app Payload/
-#zip -r Payload.zip Payload
-#mv Payload.zip Example.ipa
-
-curl -X POST "https://api-cloud.browserstack.com/app-automate/xcuitest/build" -d "{\"devices\": [\"iPhone XS-13\"], \"app\": \"bs://3d81b39af4a0bb7fb6d57800538043ca650d51a0\", \"deviceLogs\" : \"true\", \"testSuite\": \"bs://8b98463cc546241e0a39545d09081f609c852716\", \"project\": \"MQTTClient\"}" -H "Content-Type: application/json"
